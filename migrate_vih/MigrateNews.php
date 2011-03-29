91aa62a0a40d1cc142dab03da3e99181514bf439
Lars Olesen <lars@intraface.dk>
2011-03-29 19:25:24 +0200

    Now imports with full_html

--- migrate_vih/MigrateNews.php
+++ migrate_vih/MigrateNews.php
@@ -2,22 +2,38 @@
 class MigrateNews extends Migration {
   public function __construct() {
     parent::__construct();
-
-    $query = db_select('nyhed', null, array('target' => 'vih'))
-             ->fields('nyhed', array('id', 'overskrift', 'tekst'))
-             ->where('active = :active', array(':active' => 1));
+    $this->description = t('Migrate news from legacy database');
+    $this->team = array(
+      new MigrateTeamMember('Lars Olesen', 'lars@vih.dk', t('Webmaster')),
+    );
+    $query = db_select('nyhed', 'nyhed', array('target' => 'vih'))
+             ->fields('nyhed', array('id', 'overskrift', 'tekst', 'date_created', 'date_updated', 'published'))
+             ->where('nyhed.active = :active', array(':active' => 1));
+    /*
+    $query->leftJoin('keyword_x_object', 'kxo', 'nyhed.id = kxo.belong_to');
+    $query->innerJoin('keyword', 'keyword', 'keyword.id = kxo.keyword_id');
+    // Gives a single comma-separated list of related terms
+    $query->addExpression('GROUP_CONCAT(keyword.keyword)', 'terms');
+    */
     $this->source = new MigrateSourceSQL($query);
-    $this->destination = new MigrateDestinationNode('page');
+    $this->source->setMapJoinable(false);
+    $this->destination = new MigrateDestinationNode('story', array('text_format' => 'full_html'));
     $this->map = new MigrateSQLMap($this->machineName,
         array(
-          'pgid' => array('type' => 'int',
-                          'unsigned' => TRUE,
-                          'not null' => TRUE,
-                         )
+          'id' => array('type' => 'int',
+                        'unsigned' => TRUE,
+                        'not null' => TRUE,
+                       )
         ),
         MigrateDestinationNode::getKeySchema()
       );
     $this->addFieldMapping('title', 'overskrift');
     $this->addFieldMapping('body', 'tekst');
+    $this->addFieldMapping('uid')
+         ->defaultValue(1);
+    $this->addFieldMapping('created', 'date_created');
+    $this->addFieldMapping('changed', 'date_updated');
+    $this->addFieldMapping('status', 'published');
+    //$this->addFieldMapping('field_tags', 'terms')->separator(',');
   }
 }
