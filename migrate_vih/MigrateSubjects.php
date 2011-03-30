875f7fd4cd53075e25a8293d5fc8b6e5f13c1b19
Lars Olesen <lars@intraface.dk>
2011-03-30 11:37:08 +0200

    Fixed bug with break for teaser enclosed in single quotes instead of double

--- migrate_vih/MigrateSubjects.php
+++ migrate_vih/MigrateSubjects.php
@@ -10,7 +10,7 @@ class MigrateSubjects extends Migration {
     $query = db_select('langtkursus_fag', 'fag', array('target' => 'vih'))
              ->fields('fag', array('id', 'navn', 'beskrivelse', 'udvidet_beskrivelse', 'date_created', 'date_updated', 'published'))
              ->where('active = :active', array(':active' => 1));
-    $query->addExpression('CONCAT(beskrivelse, '<!--break-->', udvidet_beskrivelse)', 'concat_beskrivelse');
+    $query->addExpression('CONCAT(beskrivelse, "<!--break-->", udvidet_beskrivelse)', 'concat_beskrivelse');
     $this->source = new MigrateSourceSQL($query);
     $this->source->setMapJoinable(false);
     $this->destination = new MigrateDestinationNode('subject', array('text_format' => 'full_html'));
